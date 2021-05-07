193953808b33b16757a8431a71126dcaf82f234b
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-05-07 16:00:05 +0300

    uncommented the basic test code

--- STLExamples/src/Allocators/CustomAllocatorTests.hpp
+++ STLExamples/src/Allocators/CustomAllocatorTests.hpp
@@ -7,14 +7,14 @@ inline void CustomAllocator_Basics()
 	fmt::print("\n---------------Custom Allocator Basics--------------------------\n");
 
 
-	//std::vector<int, MyAlloc<int>> vec1{ 1,2,3,4,5 };
+	std::vector<int, MyAlloc<int>> vec1{ 1,2,3,4,5 };
 
-	//printCont(vec1);
+	printCont(vec1);
 
-	//vec1.insert(vec1.end(), { 7, 8, 9, 10, 12, 13, 4, 5, 788, 99, 80 });
+	vec1.insert(vec1.end(), { 7, 8, 9, 10, 12, 13, 4, 5, 788, 99, 80 });
 
-	//printCont(vec1);
+	printCont(vec1);
 
-	//Vec<int> vec2{ vec1 };
-	//printCont(vec2);
+	Vec<int> vec2{ vec1 };
+	printCont(vec2);
 }
