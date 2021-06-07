57a1712573893a2235eed87dd49a7e24a33ba1dc
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-06-07 16:20:59 +0300

    continued with the Parse implementation next is awaitable and awaiter that coroutine expects since we used co_await; will also add Promise type and generator and iterator

--- STLConcurrency/src/Coroutines/CortnAsyncByteStreamParser.hpp
+++ STLConcurrency/src/Coroutines/CortnAsyncByteStreamParser.hpp
@@ -2,20 +2,84 @@
 #include "ConcurrencyPCH.h"
 
 // TODO: InComplete!!!
-template<typename T, typename U>
-struct async_generator{};
 
-using FSM = async_generator<std::string, std::byte>;
+#define INCOMPLETE 0
 
-FSM Parse()
+#if INCOMPLETE
+namespace AsyncParse
 {
-	while (true)
+
+	template<typename T, typename G, typename... Bases>
+	struct promise_type_base: public Bases...
 	{
-		//std::byte b = co_await std::byte{};
+	
+	
+	
+	};
+
+	template<typename T>
+	struct awaitable_promise_type_base
+	{
+
+	};
+
+
+	template<typename T, typename U>
+	struct [[nodiscard]] async_generator
+	{
+		using promise_type = promise_type_base<T, async_generator, awaitable_promise_type_base<U> >;
+	};
+
+	using FSM = async_generator<std::string, std::byte>;
+
+	FSM Parse()
+	{
+		while (true)
+		{
+			std::byte bb = co_await std::byte{};
+
+			std::string frame{};
+
+			if (ESC == bb)
+			{
+				bb = co_await std::byte{};
+				if (SOF != bb)
+				{
+					continue; // not a start sequence 
+				}
+
+				while (true)  // try to capture the full frame
+				{
+					bb = co_await std::byte{};
+					if (ESC == bb)
+					{
+						// skip this byte look at the next one!
+						bb = co_await std::byte{};
+						if (SOF == bb)
+						{
+							// if end of frame then return the result
+							co_yield frame;
+							break;
+						}
+						else if (ESC != bb)
+						{
+							// out of sync !!!
+							break;
+						}
+					}
+
+					frame += static_cast<char>(bb);
+				}
+			}
+		}
 	}
 
 
 
 
 
-}
+
+
+} // end of namespace AsyncParse
+
+#endif // INCOMPLETE MACRO ; temporary
