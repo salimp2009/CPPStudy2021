836c09b88971eeeb6588dd73ba62f19f9db8112f
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-06-18 16:51:31 +0300

    added a std::ranges::sort example also tried std::views::split() but understood it wrong ; need to go over it; will add more ranges algorithms e.g: using Projections which uses a member functions variable to sort accordingly

--- STLRanges/src/RangesExamples01.hpp
+++ STLRanges/src/RangesExamples01.hpp
@@ -6,14 +6,24 @@ inline void RangeExample01()
 {
 	std::printf("\n---RangeExample01---\n");
 
-	std::vector numbers = { 1,2,3,4,5,6};
+	const std::vector<int> numbers = { 1,2,3,4,5,6};
 
 	auto result = numbers | std::views::filter([](int n) { return n % 2 == 0; })
 						  | std::views::transform([](int n) { return n * 2; });
+						  //| std::ranges::views::split(2);
 
 	for (auto v : result)
 	{
 		fmt::print("{} ", v);
+		
+	}
+	puts("\n");
+
+	std::vector<int> vec2 = { -3,5,0,7,-4 };
+	std::ranges::sort(vec2);
+	for (const auto elem : vec2)
+	{
+		fmt::print("{} ", elem);
 	}
 	puts("\n");
 }
