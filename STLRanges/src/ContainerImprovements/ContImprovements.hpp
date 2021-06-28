2ec668e3d48aaad51cd96ae570a83b31413bd50a
salimp2009 <46407603+salimp2009@users.noreply.github.com>
2021-06-29 01:55:39 +0300

    added more examples std::to_array by using implicit conversions, list initialization...etc; next is more std::make_shared for arrays and more container improvements such as erase_if

--- STLRanges/src/ContainerImprovements/ContImprovements.hpp
+++ STLRanges/src/ContainerImprovements/ContImprovements.hpp
@@ -28,9 +28,21 @@ inline void makeArray()
 	fmt::print("c-string: {}, element count: {}\n", arr1, arr1.size());
 
 	// creates a char array[] a c-string; each character is a seperate element of the array!
-	auto arr2 = std::to_array("testing an array");
-	fmt::print("char array: {}, element count: {} \n", arr2, arr2.size());
+	auto arr1a = std::to_array("testing an array");
+	fmt::print("char array: {}, element count: {} \n\n", arr1a, arr1a.size());
 
+	auto arr2 = std::array{ 1, 2, 3, 4, 5 };
+	fmt::print("array: {}, element count: {}\n", arr2, arr2.size());
+	
+	auto arr2a = std::to_array({ 1,2,3,4,5 });
+	fmt::print("to_array: {}, element count: {}\n\n", arr2a, arr2a.size());
+	
+	auto arr3 = std::to_array<double>({ 0,1,3 });
+	fmt::print("to_array<double>: {}, typeid: {}, size: {}\n\n", arr3, typeid(arr3[0]).name(), arr3.size());
+
+	auto arr4 = std::to_array<std::pair<int, double>>({ { 3,4.0 }, {5,6.2}, { 25,10.25 } });
+	
+	fmt::print("to_array<pair>: {}, typeid: {}, size: {}\n\n", arr4, typeid(arr4[0]).name(), arr4.size());
 
 
 }
